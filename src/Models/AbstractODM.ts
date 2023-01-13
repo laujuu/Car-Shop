6320c5fb2ada3651d4a71758329d5a99dba25f6f
laujuu <lauroindcontato@gmail.com>
2023-01-13 18:10:09 -0300

    feat: Cria a rota /cars onde é possível cadastrar um carro

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,23 @@
+import { Model, Schema, model, models } from 'mongoose';
+
+abstract class AbstractODM<T> {
+  private schema: Schema<T>;
+  private _model: Model<T>;
+  private modelName: string;
+
+  constructor(schema: Schema<T>, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this._model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  protected get model(): Model<T> {
+    return this._model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+}
+
+export default AbstractODM;
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import CarRoutes from './Routes/CarRoutes';
 
 const app = express();
+app.use(express.json());
+app.use(CarRoutes);
+app.use(ErrorHandler.handle);
 
 export default app;
